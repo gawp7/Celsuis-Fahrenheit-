a0159e53445307c4df22deccc88ad8b181cc02f5
gawp7 <ali.lazrag@gmail.com>
2025-11-18 16:02:24 +0100

    test

---
+++ tkinterentrainement.py
@@ -0,0 +1,17 @@
+from tkinter import*
+
+#Config fenêtre :
+fenetre = Tk()
+fenetre.title("Fenêtre Ali")
+fenetre.geometry("1920x1080")
+
+#Config texte :
+label = Label(fenetre, text="Test Mousmousa", font=("Arial", 40))
+label.pack(pady=350)
+#Barre de menus : 
+barre = Menu(fenetre)
+menu1 = Menu(barre, tearoff=0)
+
+
+#Afficher la page :
+fenetre.mainloop()
